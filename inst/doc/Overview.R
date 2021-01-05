3ce7f7680e7d2caa5ead5d6289bf5e5d99185549
Marcus W. Beck <mbeck@tbep.org>
2021-01-05 19:00:32 +0000

    version 2.4.0

---
+++ inst/doc/Overview.R
@@ -0,0 +1,23 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  message = FALSE, 
+  warning = FALSE,
+  comment = "#>"
+)
+
+## ----eval = F, message = F----------------------------------------------------
+#  install.packages('SWMPr')
+#  library(SWMPr)
+
+## ----eval = F, message = F----------------------------------------------------
+#  install.packages('devtools')
+#  library(devtools)
+#  install_github('fawda123/SWMPr', ref = 'development')
+#  library(SWMPr)
+
+## ----eval = F, message = F----------------------------------------------------
+#  help.search('retrieve', package = 'SWMPr')
+#  help.search('organize', package = 'SWMPr')
+#  help.search('analyze', package = 'SWMPr')
+
